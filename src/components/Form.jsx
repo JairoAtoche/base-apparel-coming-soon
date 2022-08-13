61f9ec5aeb30f1a2b4efdca0f2ec2ea8f8d4c346
Jairo Atoche <jairoatoche90098@gmail.com>
2022-08-12 21:53:07 -0500

    add: readme

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import '../css/form.css';
 import arrow from '../images/icon-arrow.svg';
+import error from '../images/icon-error.svg';
 
 function Form() {
+    let myRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+/g;
+    const [classError, setClassError] =useState(false);
+    const [form, setForm] = useState({'mail':''});
+
+    const handleChange = e => {
+        setForm({
+            ...form,
+            [e.target.name]:e.target.value
+        });
+        setClassError(false)
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!myRegex.test(form.mail.toLowerCase())) {
+            console.log(form.mail.toLowerCase(),"ERROR")
+            setClassError(true);
+        } else {
+            console.log(form.mail.toLowerCase(),"BIEN")
+            setClassError(false);
+        }
+    };
+
     return(
-        <form className='form' action='#' method='POST'>
-            <input type='email' placeholder='Email address' className='form__mail'/>
+        <form className='form' action='#' method='POST' onSubmit={handleSubmit}>
+            <input type='text' name='mail' placeholder='Email address' className={`form__mail ${classError ? 'form__mail--error' : '' }`} value={form.mail} onChange={handleChange} />
+            <img src={error} alt='' className={`${classError ? 'form__icon-error' : 'disabled' }`}/>
             <button className='form__button'>
                 <img src={arrow} alt="" />
             </button>
+            <p className={`${classError ? 'form__p-error' : 'disabled' }`}>Please provide a valid email</p>
         </form>
     );
 }
